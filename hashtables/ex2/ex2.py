dd6330ce01c5340e32f096dbf115519a2711c4d2
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-07-12 09:48:15 -0700

    i forgot to save

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -12,12 +12,55 @@ class Ticket:
         self.destination = destination
 
 
-def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
-    route = [None] * length
+tickets = [
+  ['PIT', 'ORD'],
+  ['XNA', 'CID'],
+  ['SFO', 'BHM'],
+  ['FLG', 'XNA'],
+  [None, 'LAX'], 
+  ['LAX', 'SFO'],
+  ['CID', 'SLC'],
+  ['ORD', None],
+  ['SLC', 'PIT'],
+  ['BHM', 'FLG'],
+]
 
-    """
-    YOUR CODE HERE
-    """
+def reconstruct_trip(tickets, length):
+    hash = HashTable(length)
+    # hash = {}
+    route = [None] * length
+    # route = [None] * (len(tickets)-1)
+
+
+    # for ticket in tickets:
+    #     if tickets[0] == "NONE":
+    #         route[0] = tickets[1]
+
+    #     hash[tickets[0]] = tickets[1]
+
+    # print(hash)
+    # print(route)
+
+    # for i in range(1, len(tickets)-1):
+    #     route[i] = hash[route[i-1]]
+
+    for ticket in tickets:
+
+        hash_table_insert(hash, ticket.source, ticket.destination)
+        
+        start = hash_table_retrieve(hash, 'NONE')
+
+        route[0] = start
+
+    for i, r in enumerate(route):
+
+        if i + 1 > len(route) - 1:
+            break
+
+        route[i + 1] = hash_table_retrieve(hash, r)
 
     return route
+
+# print(reconstruct_trip(tickets))
+
+
